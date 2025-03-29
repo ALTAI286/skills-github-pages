72bf345884a6430e1cd77c1733e12418879c13e4
ALTAI268 <altaikamchybekov01@gmail.com>
2025-03-29 13:50:54 +0600

    Описание изменений

---
+++ app.js
@@ -0,0 +1,14 @@
+const Buttonhead = document.querySelector("#Buttonhead")
+const home = document.querySelector(".home")
+const catalog1 = document.querySelector(".catalog")
+const programm = document.querySelector(".programm")
+
+Buttonhead.onclick = ()  => {
+    document.location.href = "./src/form/form.html"
+    home.style.display = "block"
+}
+
+
+const academy = document.querySelector(".academy")
+
+
---
+++ src/catalog/catalog.js
@@ -0,0 +1,5 @@
+const btnf = document.querySelector(".btnf");
+
+btnf.onclick = ()  => {
+    alert("Извините, заказы временно не принимаются")
+}
